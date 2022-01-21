b744ff6233e5e93d92791a7a6f89973c25fe11ca
pulidoesr <pul21010@byui.edu>
2022-01-20 23:13:54 -0600

    w03 correction of subdirrectory for pycache on dice-incomplete

--- dice-incomplete/dice/game/die.py
+++ dice-incomplete/dice/game/die.py
@@ -5,7 +5,8 @@ import random
 
 # 1) Add the class declaration. Use the following class comment.
 
-""" A small cube with a different number of spots on each of its six sides.
+class Die:
+    """ A small cube with a different number of spots on each of its six sides.
 
         The responsibility of Die is to keep track of the side facing up and calculate the points for 
         it.
@@ -17,23 +18,21 @@ import random
  
 
 # 2) Create the class constructor. Use the following method comment.
-"""Constructs a new instance of Die with a value and points attribute.
+    """Constructs a new instance of Die with a value and points attribute.
 
         Args:
             self (Die): An instance of Die.
-"""
-
+    """
+    def __init__(self):
+        self.spots = 0
+        self.points = 0
 
 # 3) Create the roll(self) method. Use the following method comment.
-"""Generates a new random value and calculates the points.
+    """Generates a new random value and calculates the points.
         
         Args:
             self (Die): An instance of Die.
         """
-
-class Die: 
-    def __init__(self):
-        self.spots = 0
-        self.points = 0
+    
     def roll(self):
         self.value = random.randint(1,6)
